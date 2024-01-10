a2a902e22e4372e8017487d29a4dbedbe00ffa9c
seok-pyo <hongseokpyou@gmail.com>
2024-01-11 01:06:08 +0900

    BFS graph search

--- it_career_algorithm/it_career_algorithm/main.cpp
+++ it_career_algorithm/it_career_algorithm/main.cpp
@@ -2,33 +2,52 @@
 #include <vector>
 #include <queue>
 using namespace std;
-vector<int> map[21];
-queue<int> Q;
-int ch[21], res[21];
+int map[22][22];
+
+int dx[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dy[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+struct Loc {
+	int x;
+	int y;
+	Loc(int a, int b) {
+		x = a;
+		y = b;
+	}
+};
+queue<Loc> Q;
 
 int main() {
-	int v, e, i, x, cnt=0, a, b, tmp;
-	cin >> v >> e;
-	for (i = 0; i < e; i++) {
-		cin >> a >> b;
-		map[a].push_back(b);
+	int n, i, j, k, num, cnt = 0;
+	cin >> n;
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			cin >> num;
+			map[i][j] = num;
+		}
 	}
-	Q.push(1);
-	res[1] = 0;
-	while(!Q.empty()){
-		tmp = Q.front();
-		Q.pop();
-		for (i = 0; i < map[tmp].size(); i++) {
-			if (res[map[tmp][i]] == 0) { // 최단거리로 한번 기록이 되면, 그 거리가 최단 거리가 된다.
-				// 중복으로 queue에 들어가는 것도 체크한다.
-				Q.push(map[tmp][i]);
-				res[map[tmp][i]] = res[tmp] + 1;
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			if (map[i][j] == 1) {
+				/*Loc tmp = Loc(i, j);
+				Q.push(tmp);*/ // Q.push(Loc(i, j) 로 바로 입력하면 된다.
+				Q.push(Loc(i, j));
+				map[i][j] = 0;
+				while (!Q.empty()) {
+					Loc x = Q.front();
+					Q.pop();
+					for (k = 0; k < 8; k++) {
+						if (map[x.x + dx[k]][x.y + dy[k]] == 1) {
+							Q.push(Loc(x.x + dx[k], x.y + dy[k]));
+							map[x.x + dx[k]][x.y + dy[k]] = 0;
+						}
+					}
+				}
+				cnt++;
 			}
 		}
 	}
-	cout << endl;
-	for (i = 2; i <= v; i++) {
-		cout << i << ":" << res[i] << endl;
-	}
+	std::cout << cnt << endl;
+
 	return 0;
 }

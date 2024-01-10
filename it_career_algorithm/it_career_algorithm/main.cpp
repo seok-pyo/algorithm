4e619ef15b1a95895066369c428f1dc069ee353e
seok-pyo <hongseokpyou@gmail.com>
2024-01-10 19:24:20 +0900

    graph shortest distance BFS

--- it_career_algorithm/it_career_algorithm/main.cpp
+++ it_career_algorithm/it_career_algorithm/main.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
-vector<int> map[10];
-int Q[10];
-int ch[10];
-int front = -1, back = -1;
+vector<int> map[21];
+queue<int> Q;
+int ch[21], res[21];
 
 int main() {
-	int a, b, i, x;
-	for (i = 0; i < 6; i++) {
+	int v, e, i, x, cnt=0, a, b, tmp;
+	cin >> v >> e;
+	for (i = 0; i < e; i++) {
 		cin >> a >> b;
-		map[a].push_back(b); // 양방향 그래프
-		map[b].push_back(a);
+		map[a].push_back(b);
 	}
-	Q[++back] = 1;
-	ch[1] = 1;
-	while (front < back) {
-		x = Q[++front];
-		cout << x << " ";
-		for (i = 0; i < map[x].size(); i++) {
-			if (ch[map[x][i]] == 0) {
-				ch[map[x][i]] = 1;
-				Q[++back] = map[x][i];
+	Q.push(1);
+	res[1] = 0;
+	while(!Q.empty()){
+		tmp = Q.front();
+		Q.pop();
+		for (i = 0; i < map[tmp].size(); i++) {
+			if (res[map[tmp][i]] == 0) { // 최단거리로 한번 기록이 되면, 그 거리가 최단 거리가 된다.
+				// 중복으로 queue에 들어가는 것도 체크한다.
+				Q.push(map[tmp][i]);
+				res[map[tmp][i]] = res[tmp] + 1;
 			}
 		}
+	}
+	cout << endl;
+	for (i = 2; i <= v; i++) {
+		cout << i << ":" << res[i] << endl;
 	}
 	return 0;
 }

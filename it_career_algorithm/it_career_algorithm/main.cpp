394453bae9cc26d21baa7a9b1dc3a483bc5f1676
seok-pyo <hongseokpyou@gmail.com>
2024-01-13 18:57:10 +0900

    도전과제: 돌다리 건너기(Bottom-up)

--- it_career_algorithm/it_career_algorithm/main.cpp
+++ it_career_algorithm/it_career_algorithm/main.cpp
@@ -6,20 +6,19 @@
 using namespace std;
 
 int dy[50];
-int DFS(int n) {
-	if (dy[n] > 0) return dy[n]; // 메모이제이션 가지 정리
-
-	if (n == 1 || n == 2) return n; // 직관적으로 알 수 있는 경우는 바로 리턴한다.
-	// 케이블을 자르는 마지막이 1인 경우와 2인 경우
-	else return dy[n] = DFS(n - 2) + DFS(n - 1);
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
-	ifstream cin;
+	cin.tie(NULL);
+	fstream cin;
 	cin.open("input.txt");
 	int n;
 	cin >> n;
-	cout << DFS(n);
+	dy[1] = 1;
+	dy[2] = 2;
+	for (int i = 3; i <= n; i++) {
+		dy[i] = dy[i - 2] + dy[i - 1];
+	}
+	cout << dy[n];
 	return 0;
 }

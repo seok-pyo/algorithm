b1ccd4cdf2de6aabc3d03a4aa5e89128bf15d8af
seok-pyo <hongseokpyou@gmail.com>
2024-01-14 12:21:41 +0900

    clear

--- it_career_algorithm/it_career_algorithm/main.cpp
+++ it_career_algorithm/it_career_algorithm/main.cpp
@@ -8,17 +8,5 @@ using namespace std;
 int dy[50];
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	fstream cin;
-	cin.open("input.txt");
-	int n;
-	cin >> n;
-	dy[1] = 1;
-	dy[2] = 2;
-	for (int i = 3; i <= n; i++) {
-		dy[i] = dy[i - 2] + dy[i - 1];
-	}
-	cout << dy[n];
-	return 0;
+	
 }

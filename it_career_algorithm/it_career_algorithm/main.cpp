850c1b27b51bafda46351ff90413b8082827573d
seok-pyo <hongseokpyou@gmail.com>
2024-01-15 16:33:38 +0900

    clear

--- it_career_algorithm/it_career_algorithm/main.cpp
+++ it_career_algorithm/it_career_algorithm/main.cpp
@@ -1,18 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <queue>
 #include <fstream>
 using namespace std;
 
-int arr[1001];
-int dy[1001];
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	/*fstream cin;
-	cin.open("input.txt");*/
-	
-	return 0;
-}

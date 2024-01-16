e9f6cc798edf7daad8d5f642bd9a21934b45dbaf
seok-pyo <hongseokpyou@gmail.com>
2024-01-17 00:11:42 +0900

    최대점수 구하기(냅색 알고리즘)

--- it_career_algorithm/it_career_algorithm/main.cpp
+++ it_career_algorithm/it_career_algorithm/main.cpp
@@ -5,7 +5,20 @@
 using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
-	
+	int n, m, s, t;
+	cin >> n >> m;
+	vector<int> dy(m + 1, 0);
+	for (int i = 1; i <= n; i++) {
+		cin >> s >> t;
+		if (dy[t] < dy[t] + s) {
+			dy[t] = dy[t] + s;
+		}
+		for (int j = 1; j <= m; j++) {
+			if (dy[j] != 0 && j + t <= m) 
+				dy[j+t] = max(dy[j + t], dy[j] + s);
+		}
+	}
+	cout << dy[m];
 	return 0;
 }
 

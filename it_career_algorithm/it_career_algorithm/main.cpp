de96a1dbd41f4bfa07e63a3253fc62bbe7124b56
seok-pyo <hongseokpyou@gmail.com>
2024-01-16 21:10:05 +0900

    동전 교환

--- it_career_algorithm/it_career_algorithm/main.cpp
+++ it_career_algorithm/it_career_algorithm/main.cpp
@@ -5,7 +5,29 @@
 using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
-	
+	int n, v, m;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v;
+		arr.push_back(v);
+	}
+	cin >> m;
+	vector<int> dy(m + 1, 0);
+	for (int i = 0; i < n; i++) {
+		v = arr[i];
+		for (int j = v; j <= m; j++) {
+			if (dy[j] == 0) {
+				dy[j] = 1;
+			}
+			else {
+				if (dy[j] <= dy[j - v] + 1) {
+					dy[j] = dy[j - v] + 1;
+				}
+			}
+		}
+	}
+	cout << dy[m];
 	return 0;
 }
 

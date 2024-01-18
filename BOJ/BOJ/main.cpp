94cfac86f40ec14b5f3b160d77aa4572058fe4d5
seok-pyo <hongseokpyou@gmail.com>
2024-01-19 01:02:26 +0900

    clear

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -10,45 +10,7 @@ vector<int> qs;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n, b, idx, i, tmp;
-
-	cin >> n;
-	qs.resize(n + 1);
-	for (i = 1; i <= n; i++) {
-		cin >> b;
-		qs[i] = b;
-		arr.push_back(i);
-	}
-
-	idx = arr.front();
-	arr.pop_front();
-	res.push_back(idx);
-
-	while (!arr.empty()) {
-		if (qs[idx] > 0) {
-			for (i = 0; i < qs[idx] - 1; i++) {
-				tmp = arr.front();
-				arr.pop_front();
-				arr.push_back(tmp);
-			}
-			idx = arr.front();
-			arr.pop_front();
-		}
-		else {
-			for (i = 0; i < -qs[idx]; i++) {
-				tmp = arr.back();
-				arr.pop_back();
-				arr.push_front(tmp);
-			}
-			idx = arr.front();
-			arr.pop_front();
-		}
-		res.push_back(idx);
-	}
-
-	for (i = 0; i < res.size(); i++) {
-		cout << res[i] << " ";
-	}
+	
 
 	return 0;	
 }

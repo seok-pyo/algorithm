af0ebc343a41ec9f2cd55a9b2ed27441a132659c
seok-pyo <hongseokpyou@gmail.com>
2024-01-18 13:51:23 +0900

    clear

--- it_career_algorithm/it_career_algorithm/main.cpp
+++ it_career_algorithm/it_career_algorithm/main.cpp
@@ -5,16 +5,7 @@
 using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
-	int n, m, s, t;
-	cin >> n >> m;
-	vector<int> dy(m + 1, 0);
-	for (int i = 1; i <= n; i++) {
-		cin >> s >> t;
-		for (int j = m; j >= t; j--) {
-			dy[j] = max(dy[j], dy[j-t] + s);
-		}
-	}
-	cout << dy[m];
+	
 	return 0;
 }
 

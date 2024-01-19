f61c053ef5b8308c8b5a5914670ca20732edde9d
seok-pyo <hongseokpyou@gmail.com>
2024-01-19 19:24:28 +0900

    약수의 합

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
-#include <fstream>
-#include <algorithm>
-#include <cmath>
+#include <vector>
 using namespace std;
-
+const int MAX = 1000000;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n;
-	
+
+	vector<long long> d(MAX + 1, 1);
+
+	for (int i = 2; i <= MAX; i++) {
+		for (int j = 1; i * j <= MAX; j++) {
+			d[i * j] += i;
+		}
+	}
+
+	vector<long long> s(MAX + 1);
+	for (int i = 1; i <= MAX; i++) {
+		s[i] = s[i - 1] + d[i];
+	}
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		cout << s[n] << '\n';
+	}
+
 	return 0;	
 }

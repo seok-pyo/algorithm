47c1c93bbc3d85ee4ae0607a62b0ab149c4a11ad
seok-pyo <hongseokpyou@gmail.com>
2024-01-19 15:56:52 +0900

    clear

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+	
 	
 	return 0;	
 }

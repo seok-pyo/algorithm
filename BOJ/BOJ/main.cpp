a529fb57309f759738f5701373cb9cb00b7fc72b
seok-pyo <hongseokpyou@gmail.com>
2024-01-19 13:07:50 +0900

    1 solve

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <deque>
 #include <fstream>
 using namespace std;
-deque<int> arr;
-vector<int> res;
-vector<int> qs;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n;
-	cin >> n;
 	int a = 0;
-	int cnt = 0;
-	while (1) {
-		a = (10*(a) % n + 1) % n;
-		cnt++;
-		if (a == 0) {
-			break;
+	int n;
+	while (cin >> n) {
+		for (int i = 1;; i++) {
+			a = (10*(a) + 1) % n;
+			if (a == 0) {
+				cout << i << '\n';
+				break;
+			}
 		}
 	}
-	cout << cnt;
 	return 0;	
 }

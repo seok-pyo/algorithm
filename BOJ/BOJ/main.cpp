ab9075080b9f993dedf2c3324d41fd96a5637888
seok-pyo <hongseokpyou@gmail.com>
2024-01-19 12:41:28 +0900

    1

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -10,7 +10,17 @@ vector<int> qs;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	
-
+	int n;
+	cin >> n;
+	int a = 0;
+	int cnt = 0;
+	while (1) {
+		a = (10*(a) % n + 1) % n;
+		cnt++;
+		if (a == 0) {
+			break;
+		}
+	}
+	cout << cnt;
 	return 0;	
 }

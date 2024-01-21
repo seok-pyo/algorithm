f2f34aa37ddf0b6a3b73c1f59bf0513f84680e7e
seok-pyo <hongseokpyou@gmail.com>
2024-01-21 16:09:07 +0900

    골드바흐의 추측

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+const int MAX = 1000000;
+int prime[MAX];
+int len = 0;
+bool chk[MAX + 1];
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	
+	for (int i = 2; i <= MAX; i++) {
+		if (chk[i] == false) {
+			prime[len++] = i;
+			for (int j = i + i; j <= MAX; j += i) {
+				chk[j] = true;
+			}
+		}
+	}
+
+	while (true) {
+		int num;
+		cin >> num;
+		if (num == 0) {
+			break;
+		}
+		for (int i = 1; i < len; i++) {
+			if (chk[num - prime[i]] == false) {
+				cout << num <<  " = " <<  prime[i] << " + " << num - prime[i] << "\n";
+				break;
+			}
+		}
+	}
 
 	return 0;	
 }

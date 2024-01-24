b7a2353e14fe83d51066ffbb13b0b590b71021b9
seok-pyo <hongseokpyou@gmail.com>
2024-01-24 13:41:53 +0900

    -1 ...

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-const int MAX = 40000;
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	
+	int t;
+	cin >> t;
+	while(t--){
+		int m, n, x, y;
+		cin >> m >> n >> x >> y;
+		x -= -1;
+		y -= -1;
+		bool ok = false;
+		for (int k = x; k < (n * m); k+=m) {
+			if (k % n == y) {
+				cout << k + 1 << '\n';
+				ok = true;
+				break;
+			}
+		}
+		if (!ok) {
+			cout << -1 << '\n';
+		}
+	}
 	return 0;
 }

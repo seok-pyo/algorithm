6564b6b493916d1ee0982eee5f0dcb0080837ef2
seok-pyo <hongseokpyou@gmail.com>
2024-01-25 11:31:45 +0900

    카잉 달력-나머지 연산 -1

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -9,12 +9,20 @@ int main() {
 	while(t--){
 		int m, n, x, y;
 		cin >> m >> n >> x >> y;
-		x -= -1;
-		y -= -1;
+		// x와 y에서 1씩 빼고, k에 1을 더해주는 방식이 더 좋다.
+		// 나머지 연산으로 다 적용하기
+		/*x -= 1;
+		y -= 1;*/
 		bool ok = false;
-		for (int k = x; k < (n * m); k+=m) {
+		for (int k = x; k <= (n * m); k+=m) {
+			if (k % n == y % n) {
+				cout << k << '\n';
+				ok = true;
+				break;
+			}
+
 			if (k % n == y) {
-				cout << k + 1 << '\n';
+				cout << k << '\n';
 				ok = true;
 				break;
 			}

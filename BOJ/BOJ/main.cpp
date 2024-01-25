71455392338cf46ce9728961c027c544c34491a3
seok-pyo <hongseokpyou@gmail.com>
2024-01-25 14:25:30 +0900

    n과 m(1)

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+int n, m;
+int arr[9];
+int ans[9];
+void DFS(int L) {
+	if (L == m) {
+		for (int i = 0; i < m; i++) {
+			cout << ans[i] << " ";
+		}
+		cout << '\n';
+	}
+	else {
+		for (int i = 1; i <= n; i++) {
+			if (arr[i] == 0) {
+				arr[i] = 1;
+				ans[L] = i;
+				DFS(L + 1);
+				arr[i] = 0;
+			}
+		}
+	}
+}
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
+	cin >> n >> m;
+	DFS(0);
 
 	return 0;
 }

594ae46a6156a16307bd5482a5b51121b383d144
seok-pyo <hongseokpyou@gmail.com>
2024-01-25 13:32:00 +0900

    clear

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -6,15 +6,6 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	int n;
-	cin >> n;
-	long long ans = 0;
-	for (int start = 1, len = 1; start <= n; start *= 10, len++) {
-		int end = start * 10 - 1;
-		if (end > n) {
-			end = n;
-		}
-		ans += (long long)(end - start + 1) * len;
-	}
-	cout << ans << '\n';
+	
 	return 0;
 }

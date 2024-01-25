c1fd795eaf96f369641e1a5673984cba4661978f
seok-pyo <hongseokpyou@gmail.com>
2024-01-25 13:25:25 +0900

    수 이어붙이기(정답)

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -5,7 +5,16 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	
-
+	int n;
+	cin >> n;
+	long long ans = 0;
+	for (int start = 1, len = 1; start <= n; start *= 10, len++) {
+		int end = start * 10 - 1;
+		if (end > n) {
+			end = n;
+		}
+		ans += (long long)(end - start + 1) * len;
+	}
+	cout << ans << '\n';
 	return 0;
 }

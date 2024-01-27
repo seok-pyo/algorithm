5f9cbfe6c32fa23972e91857d3ae5dbe858af600
seok-pyo <hongseokpyou@gmail.com>
2024-01-27 18:26:38 +0900

    퇴사, 2^n

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -3,10 +3,29 @@
 #include <vector>
 #include <queue>
 using namespace std;
+int n, ans=-2147000000;
+void DFS(int L, int sum, vector<pair<int, int>>& arr) {
+	if (L > n) return;
+	if (L == n) {
+		if (sum > ans) ans = sum;
+	}
+	else {
+		DFS(L + arr[L].first, sum + arr[L].second, arr);
+		DFS(L+1, sum, arr);
+	}
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	
+	pair<int, int> S;
+	vector<pair<int, int>> arr;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> S.first >> S.second;
+		arr.push_back(S);
+	}
+	DFS(0, 0, arr);
+	cout << ans << '\n';
 	return 0;
 }

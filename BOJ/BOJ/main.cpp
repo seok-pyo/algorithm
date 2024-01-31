b06628aa2f7f5c398a37cfaf31ecdac0af97f752
seok-pyo <hongseokpyou@gmail.com>
2024-01-31 12:36:42 +0900

    모든 순열

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -9,29 +9,32 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	int n; 
-	/*char c;
-	string arr;*/
 	cin >> n;
 	vector<int> arr(n);
 	for (int i = 0; i < n; i++) {
-		// cin >> c;
-		// arr += c;
-		cin >> arr[i];
-	}
-	int l = n - 1;
-	while (l > 0 && arr[l - 1] >= arr[l]) l--;
-
-	if (l <= 0) {
-		cout << "-1" << '\n';
-		return 0;
+		arr[i] = i + 1;
 	}
 
-	int p = n - 1;
-	while (arr[p] <= arr[l - 1]) p--;
-	swap(arr[l - 1], arr[p]);
-	sort(arr.begin() + l, arr.end());
-	for (int i = 0; i < n; i++) {
-		cout << arr[i] << ' ';	
+	int flag = 1;
+
+	while (flag) {
+		int l = n - 1;
+		while (l > 0 && arr[l - 1] >= arr[l]) l--;
+
+
+		for (int i = 0; i < n; i++) {
+			cout << arr[i] << ' ';	
+		}
+
+		if (l <= 0) {
+			break;
+		}
+
+		int p = n - 1;
+		while (p > 0 && l > 0 && arr[p] <= arr[l - 1]) p--;
+		swap(arr[l - 1], arr[p]);
+		sort(arr.begin() + l, arr.end());
+		cout << '\n';
 	}
 	cout << '\n';
 	return 0;

0b2485c7049e389fc204543f0bd1ebe4f30c5082
seok-pyo <hongseokpyou@gmail.com>
2024-02-01 12:25:42 +0900

    n과 m(2) 복습

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -3,11 +3,34 @@
 #include <string>
 #include <vector>
 using namespace std;
+int n, m;
+int chk[10];
+void DFS(int L, vector<int> &arr, int S) {
+	if (L == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << '\n';
+	}
+	else {
+		for(int i = S; i<n; i++){
+			if (chk[i] == 0) {
+				chk[i] = 1;
+				arr.push_back(i+1);
+				DFS(L + 1, arr, i);
+				chk[i] = 0;
+				arr.pop_back();
+			}
+		}
+	}
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-
+	cin >> n >> m;
+	vector<int> arr;
+	DFS(0, arr, 0);
 	return 0;
 }
 

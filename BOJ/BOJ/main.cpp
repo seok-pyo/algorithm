e95527333d68ec3ce3e5f600797fb94f62dc704e
seok-pyo <hongseokpyou@gmail.com>
2024-02-02 17:16:21 +0900

    종이 조각-1차 시도

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -7,6 +8,25 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	
+	int w, h;
+	int ws = 0, hs = 0;
+	cin >> h >> w;
+	vector<string> paper(h);
+	for (int i = 0; i < h; i++) {
+		cin >> paper[i];
+	}
+	for (int i = 0; i<paper.size(); i++) {
+		ws += stoi(paper[i]);
+	}
+	for (int i = 0; i < w; i++) {
+		int t = 1;
+		for (int j = paper.size()-1; j >= 0;j--) {
+			hs = hs + ((paper[j][i] - '0') * t);
+			t *= 10;
+		}
+	}
+
+	if (hs < ws) cout << ws;
+	else cout << hs;
 	return 0;
 }

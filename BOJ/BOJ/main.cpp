01103325956431c4c0f4fd60cb41491c5b33397d
seok-pyo <hongseokpyou@gmail.com>
2024-02-04 00:06:14 +0900

    second

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -15,18 +15,59 @@ int main() {
 
 	cin >> n;
 
-	vector<int> oa;
-	vector<int> ea;
-	int num;
-
+	vector<int> arr(n);
+	
 	for (int i = 0; i < n; i++) {
-		cin >> num;
-
-		if (num % 2 == 0) ea.push_back(num);
-		else oa.push_back(num);
+		cin >> arr[i];
 	}
+	sort(arr.begin(), arr.end());
+
+	int odd, eve, tmp;
+	for (int i = 0; i < n; i++) {
+		int cur = arr[i];
+		odd = i, eve = i;
+		while (1) {
+			eve++;
+			if (eve >= n) break;
+			if (arr[eve] % 2 == 0) break;
+		}
+		while (1) {
+			odd++;
+			if (odd >= n) break;
+			if (arr[odd] % 2 != 0) break;
+		}
+		if (cur % 2 == 0) {
+			if (odd < n) {
+				tmp = cur - arr[odd];
+				if (tmp < 0) tmp = -tmp;
+				if (on > tmp) on = tmp;
+			}
+
+			if (eve < n) {
+				tmp = cur- arr[eve];
+				if (tmp < 0) tmp = -tmp;
+				if (en > tmp) en = tmp;
+			}
+		}
+		else {
+			if (odd < n) {
+				tmp = cur - arr[odd];
+				if (tmp < 0) tmp = -tmp;
+				if (en > tmp) en = tmp;
+			}
+
+			if (eve < n) {
+				tmp = cur - arr[eve];
+				if (tmp < 0) tmp = -tmp;
+				if (on > tmp) on = tmp;
+			}
+		}
+	}
+
+	if (on == 2147000000) on = -1;
+	if (en == 2147000000) en = -1;
 	
-	
+	cout << en << ' ' << on;
 
 	return 0;
 }

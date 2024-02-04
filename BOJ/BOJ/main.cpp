34b8ae4943de0e53f822e18ec27378f307f90083
seok-pyo <hongseokpyou@gmail.com>
2024-02-04 20:54:37 +0900

    clear

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -5,18 +5,6 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n;
-	cin >> n;
-	int prev = 1;
-	int current = 1;
-
-	for (int i = 2; i <= n; i++) {
-		int next = (prev + current) % 10007;
-		prev = current;
-		current = next;
-	}
 	
-	cout << current <<'\n';
-
 	return 0;
 }

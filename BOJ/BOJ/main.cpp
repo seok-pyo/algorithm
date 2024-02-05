997e04668c90bebcc8130074d4f26ec20025cb85
seok-pyo <hongseokpyou@gmail.com>
2024-02-05 12:29:02 +0900

    카드 구매하기-다이나믹

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -2,9 +2,26 @@
 #include <vector>
 using namespace std;
 
+int D[10001];
+int card[10001];
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	
+	int n;
+	cin >> n;
+	//vector<int> card(n + 1);
+	for (int i = 1; i <= n; i++) {
+		cin >> card[i];
+	}
+
+	//vector<int> D(n+1);
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			D[i] = max(D[i-j] + card[j], D[i]);
+		}
+	}
+
+	cout << D[n] << '\n';
+
 	return 0;
 }

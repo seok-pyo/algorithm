b659b432fc73654e1a0161316431c602285176d1
seok-pyo <hongseokpyou@gmail.com>
2024-02-08 13:28:17 +0900

    다이나믹 코드 추가, 기존 코드 문제점 확인(그리디)

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -4,10 +4,11 @@
 #include <cmath>
 using namespace std;
 
+int d[100001];
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	int n;
+	/*int n;
 	cin >> n;
 	int li = sqrt(n);
 	vector<int> dy(li+1);
@@ -21,14 +22,25 @@ int main() {
 			int moc = res / (j * j);
 			res = res % (j * j);
 			tmp += moc;
-			//if (res == 0) continue;
+			if (res == 0) continue;
 		}
-		// f(dy[i] > tmp) 
+		 f(dy[i] > tmp) 
 		dy[i] = tmp;
 	}
 	
-
 	int fa = *min_element(dy.begin()+1, dy.end());
-	cout << fa << '\n';
+	cout << fa << '\n';*/
+
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		d[i] = i;
+		for (int j = 1; j * j <= i; j++) {
+			if (d[i] > d[i - j * j] + 1) {
+				d[i] = d[i - j * j] + 1;
+			}
+		}
+	}
+	cout << d[n] << '\n';
 	return 0;
 }

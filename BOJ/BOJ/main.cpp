4df80468344a38c87b1ed30a8c0f07a7ad0a2e15
seok-pyo <hongseokpyou@gmail.com>
2024-02-10 00:41:02 +0900

    합분해, 1차 시도

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -2,10 +2,32 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
+int n, k;
+const int MOD = 1000000000;
+long long cnt = 0;
+void DFS(int L, int sum) {
+	if (L > k || sum > n) return;
+	if (L == k && sum == n) {
+		cnt = (cnt + 1) % MOD;
+		//cnt++;
+		return;
+	}
+	else {
+		for (int i = 0; i <= n; i++) {
+			DFS(L + 1, sum + i);
+			//DFS(L + 1, sum, idx);
+		}
+	}
+}
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	
+
+	cin >> n >> k;
+
+	DFS(0, 0);
+
+	cout << cnt << '\n';
+
 	return 0;
 }

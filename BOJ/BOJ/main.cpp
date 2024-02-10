1cd2f6ba45a82c80316dc6c0aae637ed0e1f1849
seok-pyo <hongseokpyou@gmail.com>
2024-02-11 01:08:57 +0900

    1,2,3 더하기 3

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -3,25 +3,32 @@
 using namespace std;
 
 long long d[1000001][4];
+long long mod = 1000000009;
 
 int main() {
-	int n = 10;
 
-	for (int i = 1; i <= n; i++) {
-		if (i >= 1) {
-			d[i][1] = d[i - 1][1] + d[i - 1][2] + d[i - 1][3];
-			if (i == 1) d[1][1] = 1;
+	long long n;
+	cin >> n;
+	while (n--) {
+		long long num;
+		cin >> num;
+		for (int i = 1; i <= num; i++) {
+			
+			if (i >= 1 && d[i][1] == 0) {
+				d[i][1] = (d[i - 1][1] + d[i - 1][2] + d[i - 1][3])%mod;
+				if (i == 1) d[1][1] = 1LL;
+			}
+			if (i >= 2 && d[i][2]==0) {
+				d[i][2] = (d[i - 2][1] + d[i - 2][2] + d[i - 2][3])%mod;
+				if (i == 2) d[2][2] = 1LL;
+			}
+			if (i >= 3&&d[i][3]==0) {
+				d[i][3] = (d[i - 3][1] + d[i - 3][2] + d[i - 3][3])%mod;
+				if (i == 3) d[3][3] = 1LL;
+			}
 		}
-		if (i >= 2) {
-			d[i][2] = d[i - 2][1] + d[i - 2][2] + d[i - 2][3];
-			if (i == 2) d[2][2] = 1;
-		}
-		if (i >= 3) {
-			d[i][3] = d[i - 3][1] + d[i - 3][2] + d[i - 3][3];
-			if (i == 3) d[3][3] = 1;
-		}
+		cout << (d[num][1] + d[num][2] + d[num][3])%mod << '\n';
 	}
 
-	cout << d[n][1] + d[n][2] + d[n][3] << '\n';
 	return 0;
 }

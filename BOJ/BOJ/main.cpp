5b20096987bee84b9009cecbb22de9c02989a1f9
seok-pyo <hongseokpyou@gmail.com>
2024-02-13 20:55:15 +0900

    가장 긴 바이토닉 수열

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -1,33 +1,44 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+
 int a[1001];
 int d[1001];
-int e[1001];
+int d2[1001];
+
 int main() {
-	int n;
+	int n, cnt = 0;
 	cin >> n;
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
 	}
+
 	for (int i = 1; i <= n; i++) {
-		// int sum = 0;
 		d[i] = 1;
 		for (int j = 1; j <= i; j++) {
-			//if(a[j] < a[i]) {
-			if(a[j] > a[i] && d[i] < d[j] + 1) {
-				// sum += a[j];
+			if(a[j] < a[i] && d[i] < d[j] + 1) {
 				d[i] = d[j] + 1;
 			}
 		}
-		// d[i] = sum;
 	}
-	int MAX = -2147000000;
-	for (int i = 1; i <= n; i++) {
-		MAX = max(MAX, d[i]);
+	for (int i = n; i >= 1; i--) {
+		d2[i] = 1;
+		for (int k = i+1; k <= n; k++) {
+			if (a[i] > a[k] && d2[i] < d2[k] + 1) {
+				d2[i] = d2[k] + 1;
+			}
+		}
 	}
 
-	 cout << MAX << '\n';
+	int ans = 0;
+
+	for (int i = 1; i <= n; i++) {
+		if (ans < d[i] + d2[i] - 1) {
+			ans = d[i] + d2[i] - 1;
+		}
+	}
+
+	cout << ans << '\n';
 	return 0;
 }
 

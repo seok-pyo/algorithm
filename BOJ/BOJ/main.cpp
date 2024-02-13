99e97d94920afde0f3f238d9e0f41d0b59cc392a
seok-pyo <hongseokpyou@gmail.com>
2024-02-13 16:02:51 +0900

    가장 감소하는 부분 수열

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -12,12 +12,12 @@ int main() {
 	}
 	for (int i = 1; i <= n; i++) {
 		// int sum = 0;
-		d[i] = a[i];
+		d[i] = 1;
 		for (int j = 1; j <= i; j++) {
 			//if(a[j] < a[i]) {
-			if(a[j] < a[i] && d[i] < d[j] + a[i]) {
+			if(a[j] > a[i] && d[i] < d[j] + 1) {
 				// sum += a[j];
-				d[i] = d[j] + a[i];
+				d[i] = d[j] + 1;
 			}
 		}
 		// d[i] = sum;

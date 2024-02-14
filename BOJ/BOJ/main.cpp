ae63969ff095cce321272f897b2f17c92b9f477f
seok-pyo <hongseokpyou@gmail.com>
2024-02-15 01:05:03 +0900

    피보나치 수열 - 타일링 문제 적용

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 int main() {
-	
+	int n;
+	cin >> n;
+	int prev = 1;
+	int current = 1;
+
+	for (int i = 2; i <= n; i++) {
+		int next = (prev * 2 + current);
+		prev = current;
+		current = next;
+	}
+
+	cout << current << '\n';
 	return 0;
 }
 

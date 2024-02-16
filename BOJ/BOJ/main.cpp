202d53e99f9adbf9e3cf336d2d6c7ea6d75e003f
seok-pyo <hongseokpyou@gmail.com>
2024-02-16 15:17:09 +0900

    clear

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -2,23 +2,7 @@
 #include <algorithm>
 using namespace std;
 
-int d[31];
-
 int main() {
-	int n;
-	cin >> n;
-	d[0] = 1;
-	d[2] = 3;
-	d[4] = 5;
-
-	for (int i = 4; i <= n; i+=2) {
-		d[i] = d[i - 2] * 3;
-		for (int j = 2; j <= i/ 2; j++) {
-			d[i] += d[i - 2*j] * 2;
-		}
-	}
-
-	cout << d[n] << '\n';
 	
 	return 0;
 }

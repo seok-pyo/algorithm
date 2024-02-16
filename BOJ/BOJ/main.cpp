050f01804a58ee81a0951a09f22e237d40b297cc
seok-pyo <hongseokpyou@gmail.com>
2024-02-16 15:48:50 +0900

    큐 1차

--- BOJ/BOJ/main.cpp
+++ BOJ/BOJ/main.cpp
@@ -1,9 +1,60 @@
 #include <iostream>
 #include <algorithm>
+#include <stdio.h>
+#include <queue>
+#include <string>
 using namespace std;
 
+queue <int> Q;
 int main() {
-	
+	string command;
+	int n;
+	int integer;
+
+	cin >> n;
+
+	while (n--) {
+		cin >> command;
+		if (command == "push") {
+			cin >> integer;
+			Q.push(integer);
+		}
+		else if (command == "pop") {
+			if (Q.size() == 0) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << Q.front() << '\n';
+				Q.pop();
+			}
+		}
+		else if (command == "size") {
+			cout <<  Q.size() << '\n';
+		}
+		else if (command == "empty") {
+			if (Q.empty()) {
+				cout << 1 << '\n';
+			}
+			else {
+				cout << 0 <<'\n';
+			}
+		}
+		else if (command == "front") {
+			if (Q.empty()) {
+				cout << -1 << '\n';
+			} else {
+				cout << Q.front() << '\n';
+			}
+		}
+		else if (command == "back") {
+			if (Q.empty()) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout <<  Q.back() << '\n';
+			}
+		}
+	}
 	return 0;
 }
 
